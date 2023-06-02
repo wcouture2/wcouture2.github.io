eff4e0b1f00fb0019a40b7acba35e02213c4343b
Will Couture <boxerbrothers3@gmail.com>
2023-06-02 14:03:12 -0400

    testing hosted pictures

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 monthDayLabel.innerHTML = months[date.getMonth()] + ", " + date.getDate();
 year.innerHTML = date.getFullYear();
 
+/*
 let image = document.getElementById("image");
 imageIndex = (date.getFullYear() * (date.getDate() + date.getMonth()) % 22) + 1;
 //randomIndex = Math.floor(Math.random() * 20);
 imagePath = "pictures/" + imageIndex + ".jpg";
 
 image.src=imagePath;
+*/
