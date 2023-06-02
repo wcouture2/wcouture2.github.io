d35005995014b4bc0d1ce4847dec3550c4fb0a7b
Will Couture <boxerbrothers3@gmail.com>
2023-06-02 13:30:53 -0400

    full website

---
+++ script.js
@@ -0,0 +1,14 @@
+let monthDayLabel = document.getElementById("monthDay");
+let year = document.getElementById("year");
+
+const date = new Date();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+monthDayLabel.innerHTML = months[date.getMonth()] + ", " + date.getDate();
+year.innerHTML = date.getFullYear();
+
+let image = document.getElementById("image");
+imageIndex = (date.getFullYear() * (date.getDate() + date.getMonth()) % 22) + 1;
+//randomIndex = Math.floor(Math.random() * 20);
+imagePath = "pictures/" + imageIndex + ".jpg";
+
+image.src=imagePath;
