8db0ff5dba390acbd1008ab8343a55abf7da2139
Will Couture <boxerbrothers3@gmail.com>
2023-06-02 15:36:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,6 @@ let links = [
 let image = document.getElementById("image");
 imageIndex = ((date.getFullYear() * date.getDate() + (date.getMonth())) % links.length) + 1;
 randomIndex = Math.floor(Math.random() * 20);
-imagePath = links[imageIndex];
+imagePath = links[randomIndex];
 
 image.src=imagePath;
